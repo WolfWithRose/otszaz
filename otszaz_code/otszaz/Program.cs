3d88d61b06e4053579cc11e4cf090c4d775fba72
WolfWithRose <grimm.online7@gmail.com>
2022-02-28 09:01:18 +0100

    Befejeztuk

--- otszaz_code/otszaz/Program.cs
+++ otszaz_code/otszaz/Program.cs
@@ -9,6 +9,22 @@ namespace otszaz
 {
     class Program
     {
+        static int arfuggveny(int darabszam)
+        {
+            int ar = 0;
+            for (int i = 0; i < darabszam; i++)
+            {
+                if (i > 1)
+                {
+                    ar += 400;
+                }
+                else
+                {
+                    ar += 500 - (i * 50);
+                }
+            }
+            return ar;
+        }
         static void Main(string[] args)
         {
             string[] sorok = File.ReadAllLines("penztar.txt");
@@ -47,7 +63,7 @@ namespace otszaz
             Console.WriteLine();
             Console.WriteLine("4. feladat");
             Console.Write("Adja meg egy vásárlás sorszámát! ");
-            int sorszam = int.Parse(Console.ReadLine());
+            int sorszam = int.Parse(Console.ReadLine()) - 1;
             Console.Write("Adja meg egy árucikk nevét! ");
             string arunev = Console.ReadLine();
             Console.Write("Adja meg a vásárolt darabszámot! ");
@@ -57,8 +73,36 @@ namespace otszaz
             Console.WriteLine($"Az utolsó vásárlás sorszáma: {lista.IndexOf(lista.Last(a => a.Contains(arunev))) + 1}");
             Console.WriteLine($"{lista.Count(a => a.Contains(arunev))} vásárlás során vettek belőle.");
             Console.WriteLine();
-            Console.WriteLine("6. feladat");
+            Console.WriteLine("6. feladat");            
+            Console.WriteLine($"{darabszam} darab vételekor fizetendő: {arfuggveny(darabszam)}");
+            Console.WriteLine();
+            Console.WriteLine("7. feladat");
+            List<string> kosar = new List<string>();
+            for (int i = 0; i < lista[sorszam].Count(); i++)
+            {
+                if (!kosar.Contains(lista[sorszam][i]))
+                {
+                    Console.WriteLine($"{lista[sorszam].Count(x => x == lista[sorszam][i])} {lista[sorszam][i]}");
+                    kosar.Add(lista[sorszam][i]);
+                }                
+            }
 
+            System.IO.StreamWriter f = new StreamWriter("osszeg.txt");
+            for (int j = 0; j < lista.Count(); j++)
+            {
+                kosar = new List<string>();
+                int ar = 0;
+                for (int i = 0; i < lista[j].Count(); i++)
+                {
+                    if (!kosar.Contains(lista[j][i]))
+                    {
+                        ar += arfuggveny(lista[j].Count(x => x == lista[j][i]));
+                        kosar.Add(lista[j][i]);
+                    }
+                }
+                f.WriteLine($"{j + 1}: {ar}");
+            }
+            f.Close();
         }
     }
 }
